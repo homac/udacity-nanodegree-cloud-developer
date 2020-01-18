c21006b83104e89f66ed653a6c845cd68c8dfa78
Holger Macht <hmacht@emlix.com>
2020-01-18 15:53:39 +0100

    change CORS to accept *

--- src/server.ts
+++ src/server.ts
@@ -15,12 +15,12 @@ const c = config.dev;
 
   const app = express();
   const port = process.env.PORT || 8080; // default port to listen
-  
+
   app.use(bodyParser.json());
 
   //CORS Should be restricted
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", c.url);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
@@ -31,7 +31,7 @@ const c = config.dev;
   app.get( "/", async ( req, res ) => {
     res.send( "/api/v0/" );
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
