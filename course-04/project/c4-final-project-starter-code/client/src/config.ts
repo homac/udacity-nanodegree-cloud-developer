f907bc11864517526c1ace88364a44b46bb15457
Holger Macht <holger.macht@xing.com>
2020-01-22 13:15:10 +0100

    new apiId

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '7f792gmhgl'
+const apiId = 'emwfulyu7j'
 export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
 
 export const authConfig = {
